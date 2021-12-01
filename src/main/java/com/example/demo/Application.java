ae855804f98bb545660e94e8cbaa80ec5f6d91ba
Sachin Pachpute <sachin.pachpute@sap.com>
2021-12-01 19:25:00 +0000

    My first commit

---
+++ src/main/java/com/example/demo/Application.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @GetMapping("/book")
+    public String home(){
+        return "Welcome to Spring Boot on AWS EC2 Instance";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/demo/Book.java
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class Book {
+
+    private int id;
+    private String name;
+    private double price;
+}
